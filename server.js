eacad04b722c5ca8a9493109d4c6ede8bb3885fa
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-07-17 00:50:20 +0530

    added another route

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res)=>{
     res.send('<h1>Hello World</h1>');
 });
 
+app.get('/alternate', (req, res)=>{
+    res.send('woohoo! you reached the alterate route!')
+});
+
 app.listen(3000, ()=>{
     console.log('server started on port 3000');
 });
