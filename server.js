a7aa94047575a2b224a6337bf557b22e257b0d15
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-07-17 09:41:15 +0530

    applied nodemon

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/alternate', (req, res)=>{
     res.send('woohoo! you reached the alterate route!')
 });
 
+app.get('/about', (req,res)=>{
+    res.send('My name is Rajneel, and i created this route, for testing nodemon! works fine.')
+});
+
 app.listen(3000, ()=>{
     console.log('server started on port 3000');
 });
